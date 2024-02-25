fe80aad62a63d93b03a41c7a9bbce1377715aedf
AbdulKarim018 <lackhorse.ak@gmail.com>
2024-02-26 01:15:45 +0500

    solve chapter 4

--- JavaScript/chapter_3/script.js
+++ JavaScript/chapter_3/script.js
@@ -11,16 +11,6 @@ msg.innerHTML =
   "<br>Data type of my declared variable is " +
   typeof birthYear;
 
-//   A visitor visits an online clothing store
-// www.xyzClothing.com . Write a script to store in variables
-// the following information:
-// a. Visitor’s name
-// b. Product title
-// c. Quantity i.e. how many products a visitor wants to
-// order
-// Show the following message in your browser: “John
-// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
-
 var visitorName = "John Doe";
 var productTitle = "T-shirt";
 var quantity = 5;
