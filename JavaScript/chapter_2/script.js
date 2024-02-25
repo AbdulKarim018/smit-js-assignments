b5953a6199c2a2d97b0b33b4158bf4793550e7a1
AbdulKarim018 <lackhorse.ak@gmail.com>
2024-02-26 00:53:07 +0500

    solve chapter 1 and 2

---
+++ JavaScript/chapter_2/script.js
@@ -0,0 +1,37 @@
+// 1. Declare a variable called username.
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var username;
+var myName = "Abdul Karim";
+var message;
+message = "Hello World";
+alert(message);
+
+var studentName = "Jhone Doe";
+var studentAge = "15 years old";
+var studentCourse = "Certified Web and Mobile Application Development";
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+var email = "example@example.com";
+alert("My email address is " + email);
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
